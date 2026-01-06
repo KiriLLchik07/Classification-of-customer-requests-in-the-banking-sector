b85a901cd800fae25df3dd8a5d30ec1337053004
Esakov Kirill <144825849+KiriLLchik07@users.noreply.github.com>
2026-01-06 13:23:10 +0600

    Update settings.py

--- src/config/settings.py
+++ src/config/settings.py
@@ -4,7 +4,7 @@ from pydantic import BaseSettings
 BASE_DIR = Path(__file__).resolve().parents[2]
 
 class Settings(BaseSettings):
-    project_name: str = "banking-intent-classifier"
+    project_name: str = "Classification-of-customer-requests-in-the-banking-sector"
     data_dir: Path = BASE_DIR / "data"
     model_dir: Path = BASE_DIR / "models"
     mlflow_tracking_uri: str = "http://localhost:5000"
